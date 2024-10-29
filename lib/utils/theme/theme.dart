9c2687f36e559b95c50d9e8302c396ddda7ae829
Naman2004singh <naman.2singh9@gmail.com>
2024-10-29 22:01:59 +0530

    created the text theme with many text styles

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,11 +1,24 @@
+import 'package:coffee_app/utils/theme/custom_theme/text_theme.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
   AppTheme._();
 
   static ThemeData lightTheme = ThemeData(
-    useMaterial3: true
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: textTheme.lightTextTheme,
+    primaryColor: const Color(0xff967259)
   );
-  static ThemeData darkTheme = ThemeData();
+  static ThemeData darkTheme = ThemeData(
+     useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: textTheme.darkTextTheme,
+    primaryColor: const Color(0xff967259)
+  );
 
 }
